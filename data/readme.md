8b7c53ea76d88df9b0a2339f896e3c2c930b45ad
Haodong <davidchd@outlook.com>
2024-03-20 22:31:15 -0400

    add preprocess raw documents;

--- data/readme.md
+++ data/readme.md
@@ -1,18 +1,25 @@
 # Directory for Raw Documents
 
 ## Getting Started
-1. Manually download sets from Google Drive. Put all zip files in `data/`.
-```
-data/
-  ├─ Set-1.zip
-  ├─ Set-2.zip
-  └─ ...
-```
-2. Run `unzip.py`.
+1.  Manually download zip file(s) from Google Drive.  
+    Unzip them, and put folders of all sets in `repo/origin/` as shown below.  
+
+        repo/origin/
+          ├─ Set-1
+          ├─ Set-2
+          └─ Set-3
+
+3.  Install Python depencies:  
+
+        pip install pypinyin
+
+2.  Run `unzip.py`. It will generate processed data in `repo/data/` in the format of `region/year/`.  
+    If progress management is not required at the moment or lacking dependencies, comment out **line 50** in [unzip.py](./unzip.py).
 
 ## File Structure
+After running [unzip.py](./unzip.py), you should see something like this:
 ```
-data/
+repo/data/
   ├─ shanghai
   |  ├─ 2020
   |  ├─ 2021
@@ -26,7 +33,8 @@ data/
   └─ ...
 ```
 
-## Unzip all files in a folder:
+## Useful Commands
+To unzip all files in a single directory
 ```
 unzip -O CP936 '*.zip' -d path/to/folder/
 ```
--- data/unzip.py
+++ data/unzip.py
@@ -1,6 +1,7 @@
 import os
 cwd = os.path.dirname(os.path.realpath(__file__))
 
+import subprocess, sys
 from pypinyin import lazy_pinyin as pinyin
 
 def checkFolderExist(l1, l2=None):
@@ -12,6 +13,11 @@ def checkFolderExist(l1, l2=None):
     return
 
 def convertSubFolder(sub):
+    if sub.startswith("北京"):
+        sub = sub.replace("规范性文件", "")
+        dirOut = ''.join(s.capitalize() for s in pinyin(sub[:2], style=0, errors='replace'))
+        yearOut = sub[2:]
+        return yearOut.isnumeric(), dirOut, yearOut
     formatIndex = sub.index("规范性文件")
     dirOut = ""
     if sub[:formatIndex] == "陕西":
@@ -22,29 +28,30 @@ def convertSubFolder(sub):
     return yearOut.isnumeric(), dirOut, yearOut
 
 def main():
-
-    import subprocess
-    # subprocess.run(["unzip", "-O", "CP936", "-o", "*.zip", "-d", "./origin/"], cwd=cwd, stdout=subprocess.DEVNULL)
     # assume sets in data/origin/
-    _, sets, _ = next(os.walk(os.path.join(cwd, "origin")))
+    _, sets, _ = next(os.walk(os.path.join(cwd, "../origin")))
     print(sets)
-
     # walk through all folders
     for set in sets:
-        dirs = next(os.walk(os.path.join(cwd, "origin", set)))[1]
+        dirs = next(os.walk(os.path.join(cwd, "../origin", set)))[1]
         for dir in dirs:
-            years = next(os.walk(os.path.join(cwd, "origin", set, dir)))[1]
+            years = next(os.walk(os.path.join(cwd, "../origin", set, dir)))[1]
             for year in years:
                 isSub, dirOut, yearOut = convertSubFolder(year)
                 if isSub:
-                    print(dirOut, yearOut)
+                    print(dirOut, yearOut, end=' ')
+                    sys.stdout.flush()
                     checkFolderExist(dirOut, yearOut)
-                    dirFrom = os.path.join(cwd, "origin", set, dir, year)
+                    dirFrom = os.path.join(cwd, "../origin", set, dir, year)
                     dirDest = os.path.join(cwd, dirOut, yearOut)
-                    subprocess.run(["unzip", "-O", "CP936", "-o", "*.zip", "-d", dirDest], cwd=dirFrom, stdout=subprocess.DEVNULL)
+                    # unzip files in a directory
+                    subprocess.run(["unzip", "-O", "CP936", "-n", "*.zip", "-d", dirDest], cwd=dirFrom, stdout=subprocess.DEVNULL, stderr=subprocess.STDOUT)
+                    # add to database
                     subprocess.run(["node", "analysis/ajax/manage.js", "add", dirOut, yearOut], cwd=os.path.join(cwd, ".."))
-                    # subprocess.run(["rm", "-f", "*.zip"], cwd=dirUnzip)
-
+                    # check for any embedded folders
+                    dirsEmbedded = next(os.walk(dirFrom))[1]
+                    for dirEmbedded in dirsEmbedded:
+                        subprocess.run("mv *.txt " + dirDest + '/', shell=True, cwd=os.path.join(dirFrom, dirEmbedded))
     return
 
 if __name__ == "__main__":
