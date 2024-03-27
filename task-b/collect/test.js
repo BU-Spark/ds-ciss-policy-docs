7366356a221716218834e1f96c8473b22bb314f5
Haodong Chen <davidchd@outlook.com>
2024-03-28 05:14:01 +0800

    debug too much req;

--- task-b/collect/test.js
+++ task-b/collect/test.js
@@ -1,9 +1,14 @@
 const fs = require('fs');
 const path = require('node:path');
+<<<<<<< HEAD
 
 const axios = require('axios');
 const cheerio = require('cheerio');
 const ProxyAgent = require('https-proxy-agent').HttpsProxyAgent;
+=======
+const axios = require('axios');
+const cheerio = require('cheerio');
+>>>>>>> 65e1712 (debug too much req;)
 const db = require('better-sqlite3')(path.join(__dirname, './policies.db'));
 
 const config = require(path.join(__dirname, 'config'));
@@ -11,6 +16,7 @@ const config = require(path.join(__dirname, 'config'));
 const url = 'https://www.pkulaw.com/lar/07f795f96a47fd4720da2929809ece44bdfb.html';
 const proxyRotate = new ProxyAgent(`http://${config.proxyIp}:${config.proxyPort}`);
 
+<<<<<<< HEAD
 // for(const i of [1]) {
 //     axios.get(url, {
 //         proxy: false,
@@ -40,3 +46,15 @@ const proxyRotate = new ProxyAgent(`http://${config.proxyIp}:${config.proxyPort}
 
 // console.log(db.exec('SELECT COUNT(CASE type WHEN "" THEN 1 ELSE NULL END) AS no_type, COUNT(CASE dt WHEN "" THEN 1 ELSE NULL END) AS no_dt, COUNT(1) AS count_all FROM category WHERE area="GuangXi" and year=2021;'));
 console.log(db.prepare('SELECT COUNT(CASE type WHEN \'\' THEN 1 ELSE NULL END) AS no_type, COUNT(CASE dt WHEN \'\' THEN 1 ELSE NULL END) AS no_dt, COUNT(1) AS count_all FROM category WHERE area=? and year=?;').get('GuangXi', 2021));
+=======
+// axios.get(url).then(res => {
+//     fs.writeFileSync('pkulaw1.html', res.data);
+// });
+
+const $ = cheerio.load(fs.readFileSync('pkulaw1.html', 'utf-8'));
+console.log($('#body1'));
+// console.log($('.fields').contents())
+// const fields = $('.fields');
+// const type = fields.find('li:contains("法规类别")').find('a').text();
+// const dt = fields.find('li:contains("公布日期")').find('div').text().replace("公布日期：", "");
+>>>>>>> 65e1712 (debug too much req;)
