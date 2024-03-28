f630f96e339faf3bb81831078dcb30aaf030060b
Haodong Chen <davidchd@outlook.com>
2024-03-28 11:37:10 +0800

    add proxy;

--- task-b/collect/app.js
+++ task-b/collect/app.js
@@ -1,9 +1,13 @@
 const path = require('node:path');
 const fs = require('node:fs');
 const fsPromise = require('node:fs/promises');
+
 const axios = require('axios');
+const ProxyAgent = require('https-proxy-agent').HttpsProxyAgent;
 const cheerio = require('cheerio');
 const db = require('better-sqlite3')(path.join(__dirname, './policies.db'));
+
+const config = require(path.join(__dirname, 'config'));
 
 const sqlQueryPolicy = db.prepare('SELECT filename, type FROM category WHERE id=? and area=? and year=?;');
 const sqlInsertPolicy = db.prepare('INSERT OR IGNORE INTO category VALUES(?,?,?,?,?,?,?);');
@@ -26,7 +30,7 @@ async function collectData(jArea, jYear) {
     const dir = path.join(__dirname, '../../data/', jArea, ""+jYear);
     await fsPromise.readdir(dir).then(async files => {
         let resultStream, docsDoneCount=0;
-        const reqDurations = [];
+        const reqDurations = [], reqRetryCounts = [];
         if(syncToCsv) {
             resultStream = fs.createWriteStream('./category.csv');
             writeToStream(resultStream, 'id,filename,url,type,dt\n');
@@ -34,7 +38,9 @@ async function collectData(jArea, jYear) {
         const urlReg = /https:\/\/www\.pkulaw\.com\/lar\/\w+\.html/;
         for(file of files) {
             if(Date.now() - lastLogTime > 20*60*1000) {
-                logWithTime(`WIP: ${jArea} ${jYear} Complete ${docsDoneCount}/${files.length} AvgDuration ${Math.round(reqDurations.reduce((a,b)=>a+b)/reqDurations.length)}`)
+                const avgDuration = Math.round(reqDurations.reduce((a,b)=>a+b)/reqDurations.length);
+                const avgRetryCount = Math.round(reqDurations.reduce((a,b)=>a+b)/reqDurations.length);
+                logWithTime(`WIP: ${jArea} ${jYear} Complete ${docsDoneCount}/${files.length} AvgDuration ${avgDuration} AvgRetry `);
                 lastLogTime = Date.now();
             }
             const data = fs.readFileSync(path.join(dir, file), 'utf-8');
@@ -50,48 +56,77 @@ async function collectData(jArea, jYear) {
                 }
             }
             const policy = sqlQueryPolicy.get(id, jArea, jYear);
-            if(policy && policy.filename && policy.filename === file && policy.type.split('-')[0] === '') {
+            if(policy && policy.filename && policy.filename === file && policy.type.split('-').length < 2) {
                 docsDoneCount += 1;
                 continue;
             }
-            let reqDuration = 0;
             for(const line of lines) {
-                if(line.includes('原文链接')) {
-                    const urls = line.match(urlReg);
-                    if(urls.length > 0) {
-                        const now = Date.now();
-                        await axios.get(urls[0]).then(res => {
-                            const $ = cheerio.load(res.data);
-                            const fields = $('.fields');
-                            const type = fields.find('li:contains("法规类别")').find('a').text();
-                            const dt = fields.find('li:contains("公布日期")').find('div').text().replace("公布日期：", "");
-                            sqlInsertPolicy.run(id, jArea, jYear, file, urls[0], type, dt);
-                            writeToStream(resultStream, `${id},${dir+file},${urls[0]},${type}\n`);
-                        }).catch(async err => {
-                            // check status code
-                            const errCode = err && err.response ? err.response.status : 999;
-                            if(errCode === 404) {
-                                logWithTime('ERROR: 404 on', jArea, jYear, file);
-                                sqlInsertPolicy.run(id, jArea, jYear, file, urls[0], '404-NotFound', '');
-                                writeToStream(resultStream, `${id},${dir+file},${urls[0]},404-NotFound,\n`);
-                            } else if(errCode === 567) {
-                                logWithTime('ERROR: 567 Request Frenquency', jArea, jYear, file);
-                                sqlInsertPolicy.run(id, jArea, jYear, file, urls[0], `567-ReqFreq`, '');
-                                writeToStream(resultStream, `${id},${dir+file},${urls[0]},567-ReqFreq,\n`);
-                                await new Promise(resolve => setTimeout(resolve, 2*60*1000));
-                            } else {
-                                logWithTime('ERROR: Unknow Error', err);
-                                sqlInsertPolicy.run(id, jArea, jYear, file, urls[0], `${errCode}-Unknown`, '');
-                                writeToStream(resultStream, `${id},${dir+file},${urls[0]},${errCode}-Unknown,\n`);
-                                await new Promise(resolve => setTimeout(resolve, 20*60*1000));
-                            }
-                        });
-                        reqDuration = Date.now() - now;
-                        reqDurations.push(reqDuration);
-                        await new Promise(resolve => setTimeout(resolve, Math.max(200, Math.ceil(999 - (reqDuration / 2)))));
-                        break;
-                    }
+                if(!line.includes('原文链接')) {
+                    continue;
                 }
+                const urls = line.match(urlReg);
+                if(urls.length <= 0) {
+                    continue;
+                }
+                let reqDuration = 0, reqRetry = false, reqRetryCount = 0;
+                const now = Date.now();
+                do {
+                    reqRetryCount += 1;
+                    await axios.get(urls[0], {
+                        proxy: false,
+                        httpsAgent: new ProxyAgent(`http://${config.proxyIp}:${config.proxyPort}`),
+                    }).then(res => {
+                        const $ = cheerio.load(res.data);
+                        const fields = $('#body1').find('.fields');
+                        const types = [];
+                        for(const e of fields.find('li:contains("法规类别")').find('a')) {
+                            types.push($(e).text());
+                        }
+                        const dt = fields.find('li:contains("公布日期")').find('div').text().replace("公布日期：", "");
+                        if(types.length === 0 || dt.length === 0) {
+                            if(fields.length === 0) {
+                                reqRetry = true;
+                            } else {
+                                logWithTime('ERROR: one element missing', jArea, jYear, file);
+                            }
+                        }
+                        if(!reqRetry) {
+                            sqlInsertPolicy.run(id, jArea, jYear, file, urls[0], types.join(','), dt);
+                            writeToStream(resultStream, `${id},${dir+file},${urls[0]},${types.join(',')}\n`);
+                        }
+                    }).catch(async err => {
+                        // check status code
+                        const errCode = err && err.response ? err.response.status : 999;
+                        if(errCode === 404) {
+                            logWithTime('ERROR: 404 on', jArea, jYear, file);
+                            sqlInsertPolicy.run(id, jArea, jYear, file, urls[0], '404-NotFound', '');
+                            writeToStream(resultStream, `${id},${dir+file},${urls[0]},404-NotFound,\n`);
+                        } else if(errCode === 567) {
+                            logWithTime('ERROR: 567 Request Frenquency', jArea, jYear, file);
+                            sqlInsertPolicy.run(id, jArea, jYear, file, urls[0], `567-ReqFreq`, '');
+                            writeToStream(resultStream, `${id},${dir+file},${urls[0]},567-ReqFreq,\n`);
+                            reqRetry = true;
+                        } else if (errCode === 502) {
+                            reqRetry = true;
+                        } else if(err.message.trim().toLowerCase() === 'proxy connection ended before receiving connect response') {
+                            reqRetry = true;
+                        } else {
+                            logWithTime('ERROR: Unknow Error', err);
+                            sqlInsertPolicy.run(id, jArea, jYear, file, urls[0], `${errCode}-Unknown`, '');
+                            writeToStream(resultStream, `${id},${dir+file},${urls[0]},${errCode}-Unknown,\n`);
+                        }
+                    });
+                    if(reqRetryCount > 4) {
+                        logWithTime('ERROR: too many fails', jArea, jYear, file);
+                        reqRetry = false;
+                    } else if(reqRetry) {
+                        await new Promise(resolve => setTimeout(resolve, 400));
+                    }
+                } while(reqRetry);
+                reqDuration = Date.now() - now;
+                reqDurations.push(reqDuration);
+                reqRetryCounts.push(reqRetryCount);
+                break;
             }
             docsDoneCount += 1;
         }
--- task-b/collect/test.js
+++ task-b/collect/test.js
@@ -1,18 +1,38 @@
 const fs = require('fs');
 const path = require('node:path');
+
 const axios = require('axios');
 const cheerio = require('cheerio');
-const db = require('better-sqlite3')(path.join(__dirname, './policies.db'));
+const ProxyAgent = require('https-proxy-agent').HttpsProxyAgent;
+
+const config = require(path.join(__dirname, 'config'));
 
 const url = 'https://www.pkulaw.com/lar/07f795f96a47fd4720da2929809ece44bdfb.html';
+const proxyRotate = new ProxyAgent(`http://${config.proxyIp}:${config.proxyPort}`);
 
-// axios.get(url).then(res => {
-//     fs.writeFileSync('pkulaw1.html', res.data);
-// });
+for(const i of [1]) {
+    axios.get(url, {
+        proxy: false,
+        httpsAgent: proxyRotate,
+    }).then(res => {
+        // fs.writeFileSync('pkulaw1.html', res.data);
+        const $ = cheerio.load(res.data);
+        const fields = $('#body1').find('.fields');
+        let elements = [];
+        for(const e of fields.find('li:contains("法规类别")').find('a')) {
+            elements.push($(e).text());
+        }
+        console.log(elements, elements.join(','));
+    }).catch(err => {
+        console.log(i, (err && err.response ? err.response.status : err));
+    });
+}
 
-const $ = cheerio.load(fs.readFileSync('pkulaw1.html', 'utf-8'));
-console.log($('#body1'));
-// console.log($('.fields').contents())
-// const fields = $('.fields');
-// const type = fields.find('li:contains("法规类别")').find('a').text();
+// const $ = cheerio.load(fs.readFileSync('pkulaw0.html', 'utf-8'));
+// const fields = $('#body1').find('.fields');
+// let elements = [];
+// for(const e of fields.find('li:contains("法规类别")').find('a')) {
+//     elements.push($(e).text());
+// }
 // const dt = fields.find('li:contains("公布日期")').find('div').text().replace("公布日期：", "");
+// console.log(fields.length, elements, elements.join(','), dt);
