b4cf6497c0c034cd106b5f6371f48f55ef836caa
Haodong <davidchd@outlook.com>
2024-04-22 21:21:07 -0400

    add printWithColor(...) to utils;

--- task-a/utils.py
+++ task-a/utils.py
@@ -17,4 +17,16 @@ def pre_process_without_n(text):
     paragraphs = [p for p in paragraphs if p.strip()]
     return paragraphs
 
-
+def printWithColor(doc, idxs):
+    idxs = sorted(idxs, key=lambda x: x[0])
+    overlap = True
+    while overlap:
+        overlap = False
+        for i in range(len(idxs)-1, 0, -1):
+            if idxs[i][0] < idxs[i-1][1]:
+                idxs[i-1] = (idxs[i-1][0], idxs[i][1])
+                idxs.pop(i)
+                overlap = True
+    for idx in idxs:
+        doc = f'{doc[:idx[0]]}\033[30;103m{doc[idx[0]:idx[1]+1]}\033[0m{doc[idx[1]+1:]}'
+    print(doc)
