563c79edb622a0f83dbc012dfea6911f03f70e4d
Ender_yang <yxy007@bu.edu>
2024-04-26 12:06:09 -0400

    update find_评估标准_rule_base(docs)

--- task-a/utils.py
+++ task-a/utils.py
@@ -1,4 +1,5 @@
 import re
+import cn2an
 
 def pre_process(text):
     text = re.sub(r"\n\n", "\n", text)
@@ -41,3 +42,58 @@ def standerlize_一般政策性内容(一般政策性内容):
         一般政策性内容 = [re.sub(r'\s+', '', content) for content in 一般政策性内容]
         
     return 一般政策性内容
+
+def get_end_word(paragraph, number_pattern, number_chinese_pattern):
+    end_word = None
+    try:
+        number_result = re.search(number_pattern, paragraph)
+        number_chinese_result = number_chinese_pattern.search(paragraph)
+        
+        if number_result and number_chinese_result:
+            # if both number and chinese number exist, we choose the one smaller in index
+            number_index = paragraph.index(number_result.group())
+            number_chinese_index = paragraph.index(number_chinese_result.group())
+            if number_index < number_chinese_index:
+                number_chinese_result = None
+            else:
+                number_result = None
+        
+        if number_result:
+            number = re.search(number_pattern, paragraph).group()
+            index = paragraph.index(number)
+
+            target_number = str(int(number) + 1)
+            target_number = str(target_number)
+
+            end_word = target_number + paragraph[index + len(number)]
+        
+        elif number_chinese_result:
+            number = re.search(number_chinese_pattern, paragraph).group()
+            index = paragraph.index(number)
+
+            number_int = cn2an.cn2an(number)
+            number_int += 1
+            target_number = cn2an.an2cn(number_int)
+
+            end_word = target_number + paragraph[index + len(number)]
+    except:
+        pass
+    
+    return end_word
+
+# a helper function to add the content between current sentence and the next sentence
+def add_next_sentence(paragraphs, paragraph, next_index, matched_paragraphs, match, default_reason):
+    temp = [paragraph]
+    # Check if the next sentence index is valid, if it is valid, we add the content between current sentence and the next sentence
+    if next_index < len(paragraphs):
+        temp += [paragraphs[next_index]]
+        for index, sentence in enumerate(temp):
+            if index == 0:
+                matched_paragraphs.append((sentence, match.group()))
+            else:
+                matched_paragraphs.append((sentence, default_reason))
+    # If the next sentence index is not valid, we only add the current content 
+    else:
+        matched_paragraphs.append((paragraph.strip(), match.group()))
+        
+    return matched_paragraphs
